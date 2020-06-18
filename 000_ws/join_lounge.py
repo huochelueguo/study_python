ef64e1c74b20525c4504dc141dc5f5ecbc2cb781
Administrator <651809271@qq.com>
2020-06-18 21:15:49 +0800

    加入歌房接口

--- 000_ws/join_lounge.py
+++ 000_ws/join_lounge.py
@@ -1,13 +1,17 @@
 # 导入自己的包，可能报错，忽略即可
-import client_id
+import get_client_id
+import get_token
 
 # 获取各UID的client_id
 list2 = ['u1237326482846568448', 'u1218064453223043072', 'u1585628598650592149', 'u1592478885382154380']
 # 其他文件中调用，需要文件名.方法
-print(client_id.get_clientid(list2))
+print(get_client_id.get_clientid(list2))
 
 
 # 获取各uid的token
+dict = get_token.gettoken()
+# print(type(dict))
+print(dict)
 
 
 
