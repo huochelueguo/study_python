dd713b2aaa6ea2d28df553cbf402df5d9a6dc849
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-07-02 09:27:40 +0800

    文件 练习

--- 25_模块/b_package.py
+++ 25_模块/b_package.py
@@ -3,5 +3,6 @@ import b_package
 # 导入包后，需要使用包名.模块名.方法/类等
 b_package.receive_message.receive_m('hello')
 a = b_package.send_message.Send_Message()
+
 print(f'{a}')
 print(b_package.send_message.Send_Message)
