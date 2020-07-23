f67f639698bbb47572a1ee9647941b0535b74098
Administrator <651809271@qq.com>
2020-07-23 19:57:44 +0800

    文件读取

---
+++ pythonstudy/26_文件操作/readandwrite.py
@@ -0,0 +1,22 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:readandwrite.py
+@Time:NAME.py@Time:2020/7/23 19:21
+"""
+import os
+file_path = os.getcwd()
+list_file = os.listdir()
+print(list_file)
+for file_name in list_file:
+    # file_name2 = str.split(file_name, '.')[0]
+    with open(file_name, 'r', encoding='utf-8') as f:
+        # 读取每个文件，返回为列表
+        str = f.read()
+        # print(f.read())
+    with open('write0723', 'a+', encoding='utf-8') as file:
+        file.write(str)
+
+
