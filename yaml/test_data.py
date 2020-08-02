9f90c8c92217ce6490940d09c5dbd35666390f89
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-08-02 10:38:47 +0800

    yaml

--- yaml/test_data.py
+++ yaml/test_data.py
@@ -7,10 +7,27 @@
 @Time:NAME.py@Time:2020/7/30 上午8:45
 """
 import yaml
+import pytest
+import requests
 
 with open('test_data1', 'r', encoding='utf-8') as f:
-    data = yaml.load(f, Loader=yaml.Loader)
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    # list = []
+    # list.append(data.values())
     print(data)
-    li = data['list']
-    print(li)
-    print(type(li))
+
+
+@pytest.mark.parametrize('datas', data)
+class Test_login(object):
+
+    def test_login(self, datas):
+        print(datas['url'])
+        print(datas['body'])
+        r = requests.post(url=datas['url'], data=datas['body'])
+        # print(r.json())
+        test = r.text
+        assert 'success' in test
+
+
+if __name__ == '__main__':
+    pytest.main()
