7a27a7ff3aaaeb04866b79040a0d43114ae218ce
Administrator <651809271@qq.com>
2020-08-06 21:08:44 +0800

    高阶函数

---
+++ pythonstudy/33_高阶函数/01_map.py
@@ -0,0 +1,26 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:01_map.py
+@Time:NAME.py@Time:2020/8/6 18:49
+"""
+
+# map()函数接收两个参数，一个是函数，一个是序列，map将传入的函数依次作用到序列的每个元素，并把结果作为新的list返回。
+# 使用方法：  map（函数名[无括号]，列表名）
+# 注意：Python3版本之后，map返回值为一个迭代器，需要强转成list
+List = [1, 2, 3, 4, 5, 6, 7]
+
+
+def func(x):
+    return x * x
+
+
+data = map(func, List)
+print(list(data))
+print(map(func, List))
+
+# 练习，将列表中的不规范名称按照首字母大写，其余小写输出
+li1 = ['TONY', 'PauL', 'tom']
+
---
+++ pythonstudy/33_高阶函数/02_reduce.py
@@ -0,0 +1,9 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:02_reduce.py
+@Time:NAME.py@Time:2020/8/6 20:53
+"""
+# reduce把一个函数作用在一个序列[x1, x2, x3...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算
