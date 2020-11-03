c5cbf3745c36080abbfd5269481250dc2a75a2ef
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2020-11-03 09:27:44 +0800

    lambada&reduce

--- pythonstudy/33_高阶函数/04_匿名函数.py
+++ pythonstudy/33_高阶函数/04_匿名函数.py
@@ -10,6 +10,8 @@
 # 匿名函数不需要显示地定义函数名，使用【lambda + 参数 +表达式】的方式
 # 写法：lambda [arg1 [,arg2, ... argN]] : expression
 # 可以传入多个参数，但只能有一个表达式。
+from functools import reduce
+
 li1 = [2, 44, 65, 76, 100, 111]
 
 
@@ -24,18 +26,22 @@ def num(n, func):
 print(num(li1, lambda x: x % 2 == 0))
 print(num(li1, lambda key: key * key > 100))
 
-# 传入多个参数
-l3 = [12, 4, 5, 36, 10, 111]
 
+# map与lambda函数结合
 
-def sum2(a, b):
-    l4 = []
-    for i in range(len(a)):
-        c = a[i] * b[i]
-        l4.append(c)
-    return l4
+li1 = ['TONY', 'PauL', 'tom']
+l3 = list(map(lambda key: key[0].upper()+key[1:].lower(), li1))
+print(l3)
 
-print(sum2(li1, l3))
+# filter和lambda函数结合使用
+# 输出100以内的奇数
+li = []
+for i in range(1,10):
+    li.append(i)
 
+l4 = list(filter(lambda x: x % 2 != 0, li))
+print(l4)
 
-
+# reduce和lambda函数结合使用,注意返回值为一个数，不是列表
+l5 = reduce(lambda x, y: x * y, li)
+print(f'0-10的阶乘为：{l5}')
