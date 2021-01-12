f23339e1ee930b537073adacc90cf477a52456a7
Administrator <651809271@qq.com>
2021-01-12 21:11:34 +0800

    多线程

--- pythonstudy/36_多线程/07_死锁.py
+++ pythonstudy/36_多线程/07_死锁.py
@@ -17,27 +17,34 @@ lock_wash = Lock()
 def wash():
     global num
     lock_eat.acquire()
+    print('wash中拿到eat锁')
     lock_wash.acquire()
+    print('wash中拿到wash锁')
     num += 1
-    print('在wash中给eat上锁')
     lock_eat.release()
+    print('wash中解开eat锁')
     lock_wash.release()
+    print('wash中解开wash锁')
 
 
 def eat():
     global num
-    lock_wash.acquire()     # 此线程需要给lock_wash上锁，但是由于该锁没有释放，因此我发上锁，线程阻塞
+    lock_wash.acquire()
+    print('eat中拿到wash锁')
     time.sleep(1)
-    lock_eat.acquire()
+    lock_eat.acquire()  # wash第一次执行完成后，再第一次执行eat时，由于有一个sleep，导致第二次执行wash时抢先把eat锁了，因此eat中无法给eat上座
+    print('eat中拿到eat锁')
     num += 1
-    print('在eat中给wash上锁')
     lock_wash.release()
+    print('eat中解开wash锁')
     lock_eat.release()
+    print('eat中解开eat锁')
 
 
 if __name__ == '__main__':
-    for i in range(3):
+    for i in range(2):
         t1 = Thread(target=wash, daemon=False)
         t2 = Thread(target=eat, daemon=False)
         t1.start()
         t2.start()
+        print('-' * 10)
