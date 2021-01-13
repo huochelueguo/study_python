5f29a5c4f6420e013d69eca114f29bcdb373d656
Administrator <651809271@qq.com>
2021-01-13 15:17:00 +0800

    多线程

--- pythonstudy/36_多线程/08.递归锁.py
+++ pythonstudy/36_多线程/08.递归锁.py
@@ -6,3 +6,45 @@
 @File:08.递归锁.py
 @Time:NAME.py@Time:2021/1/12 18:57
 """
+import time
+from threading import Thread, RLock
+
+num = 0
+lock_eat = RLock()
+lock_wash = RLock()
+
+
+def wash():
+    global num
+    lock_eat.acquire()
+    print('wash中拿到eat锁')
+    lock_wash.acquire()
+    print('wash中拿到wash锁')
+    num += 1
+    lock_eat.release()
+    print('wash中解开eat锁')
+    lock_wash.release()
+    print('wash中解开wash锁')
+
+
+def eat():
+    global num
+    lock_wash.acquire()
+    print('eat中拿到wash锁')
+    time.sleep(1)
+    lock_eat.acquire()  # wash第一次执行完成后，再第一次执行eat时，由于有一个sleep，导致第二次执行wash时抢先把eat锁了，因此eat中无法给eat上座
+    print('eat中拿到eat锁')
+    num += 1
+    lock_wash.release()
+    print('eat中解开wash锁')
+    lock_eat.release()
+    print('eat中解开eat锁')
+
+
+if __name__ == '__main__':
+    for i in range(2):
+        t1 = Thread(target=wash, daemon=False)
+        t2 = Thread(target=eat, daemon=False)
+        t1.start()
+        t2.start()
+        print('-' * 10)
