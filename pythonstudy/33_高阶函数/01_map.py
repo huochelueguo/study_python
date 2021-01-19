04b148e0d699bc83ed2f70d407e98414a5fb78e6
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-01-19 09:37:18 +0800

    map practice

--- pythonstudy/33_高阶函数/01_map.py
+++ pythonstudy/33_高阶函数/01_map.py
@@ -37,3 +37,12 @@ print(list(li2))    # ['Tony', 'Paul', 'Tom']
 print(list(li2))    # [] 由于迭代器只能迭代一次，因此在此输出时为空
 
 
+# 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']：
+l1 = ['adam', 'LISA', 'barT']
+def normalize(name):
+    return name[0].upper() + name[1:].lower()
+
+# 测试:
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
