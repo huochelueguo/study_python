cad9f61d6075a2a5cc1fbfd9c8f111046e8a172f
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-01-25 23:10:23 +0800

    __mro__

--- pythonstudy/43_魔术方法/3.__mro__.py
+++ pythonstudy/43_魔术方法/3.__mro__.py
@@ -6,3 +6,38 @@
 @File:3.__mro__.py
 @Time:NAME.py@Time:2021/1/25 21:19
 """
+
+class a():
+
+    def test(self):
+        print(1)
+
+
+class b(a):
+
+    def test(self):
+        print('enter b')
+        super().test()
+        print('leave b')
+
+
+class c(a):
+
+    def test(self):
+        print('enter c')
+        super().test()
+        print('leave c')
+
+
+class d(b, c):
+
+    def test(self):
+        print('enter d')
+        super().test()
+        print('leave d')
+
+
+if __name__ == '__main__':
+    print(d.__mro__)    # 多继承时，输出执行顺序
+    dd = d()
+    dd.test()
