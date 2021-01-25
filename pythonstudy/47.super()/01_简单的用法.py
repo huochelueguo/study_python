2136dd0a8088efa3e3a69683dbb93ecd029e7830
sunze <sunze@maetimes.com>
2021-01-25 20:36:41 +0800

    SUPER

--- pythonstudy/47.super()/01_简单的用法.py
+++ pythonstudy/47.super()/01_简单的用法.py
@@ -8,8 +8,12 @@
 """
 
 """
-super() 作用：
+super() 
+作用：
     当子类具有和父类同名的方法时，子类执行该方法将仅执行子类方法中的内容，如果同时想调用父类方法中的内容，就可以使用super().来调用
+使用方法：
+    1、构造方法中，有同名参数可以使用super().__init__(参数)
+    2、普通方法中，使用super().xx()调用父类中的xx方法   
 """
 
 class Human():
@@ -25,12 +29,28 @@ class Human():
 class Man(Human):
 
     def __init__(self, skill, age, name):
-        super().__init__(age, name)
+        super().__init__(age, name) # 调用父类init中的age和name
         self.skill = skill
 
     def paly(self):
-        super().paly()
-        print(self.skill)
+        super().paly()  # 调用父类的play方法
+        print(f'技能是{self.skill},年龄是{self.age},姓名为{self.name}')
 
-man = Man(skill='抽烟', age=20, name='q')
-man.paly()
+
+class Women(Human):
+
+    def __init__(self, skill, age, name):
+        super().__init__(age, name)
+        self.name = name * 10   # 自己的类中进行初始化数据，因此name和age值和父类中不一致
+        self.age = age - 1
+        self.skill = skill
+
+    def paly(self):
+        print(f'技能是{self.skill},年龄是{self.age},姓名为{self.name}')
+
+
+if __name__ == '__main__':
+    man = Man(skill='抽烟', age=50, name='q')
+    man.paly()
+    women = Women(skill='KTV', age=100, name='q')
+    women.paly()
---
+++ pythonstudy/47.super()/02.菱形继承.py
@@ -0,0 +1,8 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:02.菱形继承.py
+@Time:NAME.py@Time:2021/1/25 20:36
+"""
