9edd7d926432a8c2d4bc22f46ec945b7d4ccb2e3
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-01-26 09:20:24 +0800

    __new__

--- pythonstudy/43_魔术方法/2.__new__和__init__.py
+++ pythonstudy/43_魔术方法/2.__new__和__init__.py
@@ -8,7 +8,7 @@
 """
 # __new__返回值：
 # 1.返回为None,此时无法调用构造方法
-# 2.返回为其他值，
+# 2.返回为其他类实例，不会执行本类的构造方法
 # 3.返回为该类实例，正常逻辑，能够正常走流程
 
 class A():
@@ -42,7 +42,7 @@ print('*' * 20)
 class C():
     def __new__(cls, *args, **kwaargs):
         print(5)
-        return b
+        return b    # 返回其他类的实例，因此不会执行下方的构造方法
 
     def __init__(self):
         print(6)
