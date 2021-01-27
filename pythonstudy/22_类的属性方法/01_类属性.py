8f38d8f3f9e609f3451a078ebce7cc719aa6e0b0
sunze <sunze@maetimes.com>
2021-01-27 18:54:31 +0800

    类属性和类方法

--- pythonstudy/22_类的属性方法/01_类属性.py
+++ pythonstudy/22_类的属性方法/01_类属性.py
@@ -1,1 +1,23 @@
-pass
+class A(object):
+
+    a = 0   # 公有类属性
+    _b = 0  # 私有类属性
+
+    def __init__(self, num):
+        self.num = num
+
+    def count_num(self):
+        A.a += 1    # 类属性只能通过类或者类的实例来调用
+        A._b += 1
+        return A.a, A._b
+
+    def siyou(self):
+        return A._b * 10
+
+
+for i in range(10):
+    a = A(i).count_num()
+    print(a)
+b = A(2)
+print(b.siyou())    # 通过类的方法间接调用类的私有属性
+print(b.a)
---
+++ pythonstudy/22_类的属性方法/02_类方法.py
@@ -0,0 +1,29 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:02_类方法.py
+@Time:NAME.py@Time:2021/1/27 18:39
+"""
+"""
+类方法：
+    定义：和类本身有关的方法，和实例无关
+    创建：
+        方法名上带装饰器@claseemethod
+        方法括号中不是self，而是cls
+    使用场景：
+    
+"""
+
+
+class A(object):
+
+    @classmethod
+    def method1(cls):
+        print('this is a 类方法')
+
+
+a = A()
+a.method1()     # 通过类的实例.可以调用类方法
+A.method1()     # 通过类名可以直接调用类的方法
