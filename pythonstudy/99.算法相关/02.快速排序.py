0d7beb86a367a071092a0c9eaa15421b4a97faf6
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-03-01 09:52:10 +0800

    kuaipai

---
+++ pythonstudy/99.算法相关/02.快速排序.py
@@ -0,0 +1,28 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:02.快速排序.py
+@Time:2021/3/1 上午9:44
+"""
+
+def quicksort(testlist, start, end):
+    if start > end:
+        return testlist
+    else:
+        i, j = start, end
+        base = testlist[i]
+        while i < j and (testlist[j] > base):
+            j -= 1
+        testlist[i] = testlist[j]
+        while i < j and (testlist[i] < base):
+            i += 1
+        testlist[j] = testlist[i]
+        testlist[i] = base
+    quicksort(testlist, start, j-1)
+    quicksort(testlist, j+1, end)
+    return testlist
+
+lists=[30,24,5,58,18,36,12,42,39]
+print(quicksort(lists, 0, 8))
