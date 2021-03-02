8dba59ef24a4f43f6586e0a437a43f8a75f78252
sunze <sunze@maetimes.com>
2021-03-02 21:20:38 +0800

    动态规划实现最长子序列

--- pythonstudy/99.算法相关/03.最长递增子序列(动态规划).py
+++ pythonstudy/99.算法相关/03.最长递增子序列(动态规划).py
@@ -13,6 +13,7 @@
 """
 
 
+# 最长递增子序列最长值
 def lenthoflist(testlist):
     n = len(testlist)
     dp = [1] * n
@@ -23,11 +24,30 @@ def lenthoflist(testlist):
     return dp
 
 
+# 最长递增子序列列表
+def longlist(testlist, dp):
+    n = max(dp)
+    list = [0] * n
+    index = dp.index(n)
+    n-=1
+    list[n] = testlist[index]
+    for i in range(index-1, 0, -1):
+        if testlist[index] > testlist[i] and dp[index] - 1 == dp[i]:
+            n-=1
+            index = i
+            list[n] = testlist[i]
+
+
+    return list
+
+
 if __name__ == '__main__':
-    testlist = [3, 4, 1, 4, 7, 19, 10]
+    testlist = [3, 1, 4, 5, 9]
     final_list = lenthoflist(testlist)
     print(f'每个元素为终点的最长子序列值为:{final_list}')
     print(f'最长递增子序列值为:{max(final_list)}')
+    dp = final_list
+    print(longlist(testlist, dp))
 
 # 每个元素为终点的最长子序列值为:[1, 2, 1, 2, 3, 4, 4]
 # 最长递增子序列值为:4
