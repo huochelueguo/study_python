9f44dca3f9f6d3e142630fb23d811040b3e8f545
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-04-25 22:43:58 +0800

    字典

--- pythonstudy/12_字典/06_字典的遍历.py
+++ pythonstudy/12_字典/06_字典的遍历.py
@@ -1,12 +1,17 @@
 # 1.字典遍历专门的函数，字典名.items()，可以使用for循环直接遍历输出
 dict1 = {
-    "name": "zhangsan",
+    "name": 222,
     "age": 20,
-    "sex": "man",
+    "sex": 0,
     "weight": 75
 }
 
 for key, value in dict1.items():
     print(key, value)
 
+res = dict1.items() # 直接输出为dict1_items([key,value]组成的一个个元组)
+#   dict_items([('name', 'zhangsan'), ('age', 20), ('sex', 'man'), ('weight', 75)])
+print(list(res))    # 强转后生成一个list
 
+res2 = sorted(list(res), key = lambda x : x[0])
+print(dict(res2))
