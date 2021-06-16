b1e6eb207bdac9629a0c5883db3b64172f6e04ad
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-06-16 09:03:03 +0800

    装饰器实现单例

--- pythonstudy/53_池和协程/04_线程池map.py
+++ pythonstudy/53_池和协程/04_线程池map.py
@@ -20,13 +20,13 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 
 
-def test1(n):
+def teest1(n):
     time.sleep(2)
     print(f'第{n}次调用')
     return n * n
 
 
 with ThreadPoolExecutor(max_workers=10) as pool:
-    result = pool.map(test1, [i for i in range(20)])
+    result = pool.map(teest1, [i for i in range(20)])
     for i in result:
         print(i)
