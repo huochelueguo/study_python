ec6310c0abfce620952ebeb064834a559048dfea
DESKTOP-FV3BAAN\sz <sunze@maetimes.com>
2021-10-25 18:53:37 +0800

    修改错别字

--- pythonstudy/52.栈与队列/02_栈的应用.py
+++ pythonstudy/52.栈与队列/02_栈的应用.py
@@ -10,7 +10,7 @@
 """
 括号匹配问题
 https://www.cnblogs.com/superhin/p/11454799.html
-思路：利用字典和栈的思想，将括号座位一个字典，为左括号时插入到栈里面，是右括号时取出栈顶进行对比
+思路：利用字典和栈的思想，将括号作为一个字典，为左括号时插入到栈里面，是右括号时取出栈顶进行对比
 """
 
 
@@ -21,7 +21,7 @@ def is_closed(txt):
     for i in txt:
         if i in kuohao_dict.values():   # 最先开始的是左侧括号，因此如果是左括号，插入栈中
             stack.append(i)
-        elif i in kuohao_dict.keys():   # 如果是有括号，那就将i和栈中最外侧的一个值比较，如果相同，则删除最外侧，否则报false
+        elif i in kuohao_dict.keys():   # 如果是右括号，那就将i和栈中最外侧的一个值比较，如果相同，则删除最外侧，否则报false
             # if kuohao_dict[i] == stack[-1]:
             #     stack.pop()
             # else:
@@ -35,5 +35,5 @@ def is_closed(txt):
 text = "({[({{abc}})][{1}]})2([]){({[]})}[]"
 text2 = '{[]}'
 text3 = '{}{}[]'
-print(is_closed(text))
+print(is_closed(text2))
 
