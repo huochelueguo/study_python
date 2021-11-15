7afd40f61e515274c3693d70101c75e5d299cb00
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-11-15 09:39:58 +0800

    算法

---
+++ pythonstudy/99.算法相关/07. 最长公共子串.py
@@ -0,0 +1,9 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:孙泽
+@Github:https://github.com/huochelueguo
+@File:07. 最长公共子串.py
+@Time:2021/11/15 9:38 上午
+"""
+# 最长公共子串：两个字符串中连续相等的最长子串。 https://www.cnblogs.com/anzhengyu/p/11166708.html
