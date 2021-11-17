e68cdd028a6832354ddd3e90495c7a6a93ef5770
“sunwenxiao001” <sunwenxiao001@lianjia.com>
2021-11-18 00:02:07 +0800

    算法

--- pythonstudy/99.算法相关/08.最长上升子序列.py
+++ pythonstudy/99.算法相关/08.最长上升子序列.py
@@ -2,7 +2,7 @@ def longest(list):
     n = len(list)
     dp = [1] * n
     for i in range(1, n):
-        for j in range(1, i):
+        for j in range(1, i+1):
             if list[i] > list[j]:
                 dp[i] = max(dp[i], dp[j]+1)
 
