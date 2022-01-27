74c345f13115d25ca9505e0fcc4ac0f9ac433d00
DESKTOP-FV3BAAN\sz <sunze@maetimes.com>
2022-01-27 16:18:34 +0800

    完成歌房送礼ws+接口

--- Lounge/Get_Clientid.py
+++ Lounge/Get_Clientid.py
@@ -5,18 +5,17 @@
 @Github:https://github.com/huochelueguo
 @File:Get_Clientid.py
 @Time:NAME.py@Time:2020/8/26 17:17
-@歌房功能封装模块： 获取用户clientid
 """
 
 # 导入的模块是websocket_client，相似的比较多，容易混淆
 import json
-import os
-import yaml
-
 from websocket import create_connection
 
 
 class Get_Clientid(object):
+    """
+        歌房功能封装模块： 获取用户clientid
+    """
 
     def __init__(self, token, uid):
         """
@@ -29,19 +28,18 @@ class Get_Clientid(object):
         self.url = 'ws://test.api.pokekara.com/ws'
         self.ws = create_connection(url=self.url, timeout=2000, header={
             'Cookie': self.token,
-            'User-Agent':'okhttp/4.9.0'
+            'User-Agent': 'okhttp/4.9.0'
         })
-        # print(f'recv是{self.ws.recv()}')
+        self.command_9001 = self.ws.recv()
 
-    def get_id(self):
+    def get_clientid(self):
         """
         连接ws后，获取对应uid的clientid，存储在一个列表里面，后续操作中使用
+            ***注意： 此处的client_id取的是track_id，不是client_id***
         """
         if self.ws.connected:
-            res = self.ws.recv()
-            dict_res = json.loads(res)
+            dict_res = json.loads(self.command_9001)
             dict_clientid = dict_res['track_id']
-            print(dict_clientid)
             return dict_clientid
         else:
             print('连接失败，请重试')
@@ -52,20 +50,21 @@ class Get_Clientid(object):
         """
         data_json = json.dumps(data, indent=4, ensure_ascii=False)
         self.ws.send(data_json)
+        print(f'ws请求体{data}')
         a = self.ws.recv()
         print(f'ws消息回执：{a}')
 
 
 if __name__ == '__main__':
 
-    uid1 = 'u1631850899824007783'
-    token1 = 'poke_session_id=MTYzMTg1MTI0Nnx5czUxMGJTU0M4UGI2ejJRY0xwMk5aY2ptN0xZSDVFd2s1ZGh5d21OSUQzVXAxdm9STGQzeVBPaDZEc2J1VGl5Q3ZZZUtEVnBvYy01dDdxMEEzcXNmSHNSY2R6ZzJjZkd8wLEh1qb273AM9FuuVvn7Bl5b2Pj6fXfkPgOMu_fzGPk='
+    uid1 = 'u1631851507772944570'
+    token1 = 'poke_session_id=MTYzMTg1MTUwN3xtQ19YRE92eUVkMExnZldoT25uSXQ0aVZaYXB0bjNmc2d0UGdKLWh2RG9yTk1oZE9JcUtlT01yTlB2X1RVOXhpOV9yek9Gdi1Uc3BGYzlVMVQzS2dPdkFHRlBTSHVEeXF8rl6wiQ4pNO8cIjUDQBbBJBVYQgjqkR1TvM9sfKGwTTI='
     # token1 = 'poke_session_id=MTYzMTc5NzA5OHxpZWZnYnRpS2FIOVBrSGtIb3UzR3hPZkluRm9tZEFEVHExUGxnMzZ0aFdEWkJXS05ER1VCeHZudnFuWnl6clJxelBaNk45U3NkZTNFRkNQU1p0ZzFXd3Z2czVvSTF3cjd8-cgiYxBE13XVwX1PoLKLoccv9BeyafYsLejgPk6KiT4='
     user = Get_Clientid(token=token1, uid=uid1)
     data = {
         "msg_id": "e21aaf6b-ef53-11ea-9e59-5254009bf4c3",
         "data": {
-            "client_id": user.get_id(),
+            "client_id": user.get_clientid(),
             "message_id": "2F221282D-BD5F-4854-8786-67C6DCC7CC42",
             "content": "12132",
             "type": 1,
--- Lounge/Join_Lounge.py
+++ Lounge/Join_Lounge.py
@@ -18,14 +18,14 @@ import yaml
 from Lounge.Get_Clientid import Get_Clientid
 from Lounge.Read_Usertoken import Read_Uid_Token
 
-ROOM_ID = '4e5f9110-7f28-11ec-9fcb-5254002d7e9a'
+ROOM_ID = 'e6b1fe98-7f3d-11ec-9fcb-5254002d7e9a'
 CLIENTID_PATH = 'E:/pythonworkspace/python_study/Lounge/user_data/clientid'
-ROOM_NID = 1486554403070758912
+ROOM_NID = 1486593305181638656
 
 
 class Post(object):
     """
-    加房post接口
+        加房post接口
     """
 
     def __init__(self, token, client_id, room_id):
@@ -33,7 +33,7 @@ class Post(object):
         self.client_id = client_id
         self.room_id = room_id
 
-    def Post_Join(self):
+    def post_join(self):
         url = 'http://test.api.pokekara.com/x/lounge/join?'
         header = {'Content-Type': 'application/json',
                   'cookie': self.token,
@@ -48,7 +48,7 @@ class Post(object):
 
 class Join_Lounge(object):
     """
-    遍历获取数据后，调用加房接口，按照顺序加房
+        获取用户uid和token，再去请求client_id，之后去请求加房接口
     data_uid: uid列表
     data_token： token列表
     data_clientid： clientid列表
@@ -62,7 +62,7 @@ class Join_Lounge(object):
         # 获取用户clientid
         data_clientid = []
         for i in range(len(data_uid)):
-            id = Get_Clientid(data_token[i], data_uid[i]).get_id()
+            id = Get_Clientid(data_token[i], data_uid[i]).get_clientid()
             data_clientid.append(id)
             # 输出client_id到文件中，后续其他操作可能会用到
             curr_path = os.path.split(__file__)[0]
@@ -71,12 +71,11 @@ class Join_Lounge(object):
                 yaml.dump(data_clientid, f)
             # 使用clientid和token加入歌房
             try:
-                res = Post(token=data_token[i], client_id=data_clientid[i], room_id=room_id).Post_Join()
+                res = Post(token=data_token[i], client_id=data_clientid[i], room_id=room_id).post_join()
             except Exception as result:
                 print(f'{result}')
-        # print(data_clientid)
         return res
 
 
 if __name__ == '__main__':
-    Join_Lounge().join(data_file='user_1', room_id="f3b6d206-176b-11ec-8a1d-5254002d7e9a")
+    Join_Lounge().join(data_file='user_1', room_id=ROOM_ID)
