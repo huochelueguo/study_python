b7e352550b627e5ed9758131f9ef90d101da7d84
DESKTOP-FV3BAAN\sz <sunze@maetimes.com>
2022-01-28 11:06:23 +0800

    线程池评论完善

--- Lounge/Send_Message_Threading.py
+++ Lounge/Send_Message_Threading.py
@@ -11,6 +11,9 @@ import time
 import uuid
 import random
 import threading
+import os
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Process
 from Lounge.Get_Clientid import Get_Clientid
 from Lounge.Join_Lounge import Join_Lounge, ROOM_ID, CLIENTID_PATH
 from Lounge.User_Add_Clientid import Add_Clientid
@@ -23,31 +26,31 @@ message = ["こんにちは！", "よろしくお願いします！", "うまい
 
 class Send_Mess_Threading(object):
 
-    def __init__(self,  user_data, room_id, thread_count=None,):
+    def __init__(self,  user_data, room_id, thread_count=None):
         self.thred_count = thread_count
         self.user_data = user_data
         self.room_id = room_id
 
     def send_txt(self):
-        while True:
-            send_message = random.choice(message)
-            data = {
-                "msg_id": str(uuid.uuid4()),
-                "data": {
-                    "client_id": self.user_data[2],
-                    "message_id": str(uuid.uuid1()),
-                    "content": send_message,
-                    "type": 1,
-                    "room_id": self.room_id
-                },
-                "command": 4003
-            }
-            try:
-                ws = Get_Clientid(token=self.user_data[1], uid=self.user_data[0])
-                ws.send_message(data=data)
-                # print(f'{self.user_data[0]} + success')
-            except Exception as result:
-                print(f'send_txt err:{result}')
+        send_message = random.choice(message)
+        data = {
+            "msg_id": str(uuid.uuid4()),
+            "data": {
+                "client_id": self.user_data[2],
+                "message_id": str(uuid.uuid1()),
+                "content": send_message,
+                "type": 1,
+                "room_id": self.room_id
+            },
+            "command": 4003
+        }
+        try:
+            ws = Get_Clientid(token=self.user_data[1], uid=self.user_data[0])
+            ws.send_message(data=data)
+            # print(os.getpid())
+            time.sleep(0.01)
+        except Exception as e:
+            print(f'send_txt err:{e}')
 
     def send_thread(self):
         thread_list = []
@@ -55,18 +58,37 @@ class Send_Mess_Threading(object):
             thread_list.append(threading.Thread(target=self.send_txt))
         for t in thread_list:
             t.start()
-        # time.sleep(0.5)
+
+    def send_thread_pool(self, max_workers):
+        with ThreadPoolExecutor(max_workers=max_workers) as pool:
+            while True:
+                for i in range(self.thred_count):
+                    pool.submit(Send_Mess_Threading(user_data=data[i], room_id=room_id).send_txt())
 
 
 if __name__ == '__main__':
-    user_path = 'user_50'
-    thread_count = 15
+    user_path = 'user_500'
     clientid_path = CLIENTID_PATH
     room_id = ROOM_ID
     # 加入房间
     Join_Lounge().join(data_file=user_path, room_id=room_id)
     data = Add_Clientid(user_path=user_path, clientid_path=clientid_path).uid_token_clientid()
-    for i in range(thread_count):
-        Send_Mess_Threading(thread_count=thread_count, user_data=data[i], room_id=room_id).send_thread()
+
+    # 多线程发送
+    # thread_count = 50
+    # for i in range(thread_count):
+    #     Send_Mess_Threading(thread_count=thread_count, user_data=data[i], room_id=room_id).send_thread()
+
+    # 线程池发送
+    thread_count = 30    # 线程池线程数
+    send_mess_person_num = 150   # 发布评论用户数
+    Send_Mess_Threading(thread_count=send_mess_person_num, user_data=data, room_id=room_id).send_thread_pool(thread_count)
+
+
+
+
+
+
+
 
 
