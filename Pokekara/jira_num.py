64b7501c8b4f206c21696161909e7709e49d5d39
sunze <sunze@maetimes.com>
2025-12-02 16:57:21 +0800

    update

--- Pokekara/jira_num.py
+++ Pokekara/jira_num.py
@@ -7,18 +7,18 @@
 import pandas as pd
 
 # 读取Excel文件
-df = pd.read_excel('/Users/sunwenxiao/Desktop/质量报告/2025/3-4/A_JIRA.csv')  # 替换为你的文件路径
+df = pd.read_excel('/Users/sunwenxiao/Desktop/JIRA线上.xlsx')  # 替换为你的文件路径
 
 # 统计每个经办人-解决结果的组合数量
-result = df.groupby(['经办人', '解决结果']).size().unstack(fill_value=0)
+result = df.groupby(['经办人', '状态']).size().unstack(fill_value=0)
 
 # 创建透视表
 pivot = pd.pivot_table(df,
                        index='经办人',
-                       columns='解决结果',
+                       columns='状态',
                        values='主题',
                        aggfunc='count',
                        fill_value=0)
 
 print(pivot)
-pivot.to_excel('/Users/sunwenxiao/Desktop/透视表结果1.xlsx')
+pivot.to_excel('/Users/sunwenxiao/Desktop/透视表结果3.xlsx')
